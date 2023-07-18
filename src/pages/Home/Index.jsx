66d7f3a5b88789053c03ed29a4b1cd52ea952edc
Reinaldo Porto <reinaldoportoti@gmail.com>
2023-07-18 20:40:56 -0300

    estatisticas

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,10 +1,31 @@
 import { Header } from "../../components/Header/Index"
 import { Sidebar } from "../../components/Sidebar/Index"
-export const HomePage = ()=>{
+import { Estatisticas } from "../../components/Estatisticas/Index"
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { RiMedicineBottleLine } from 'react-icons/ri';
+
+export const HomePage = () => {
     return (
         <>
-            <Header/>
-            <Sidebar/>
+            <Header />
+            <Container fluid className="home">
+                <Row>
+                    <Col className="d-flex home">
+                        <Sidebar />
+                        <Col>
+                        <Row>
+                            <Col className="text-center" style={{marginTop: '30px', marginRight: 'auto'}}><h2>Estatisticas do Sistema</h2></Col>
+                        </Row>
+                            <Estatisticas quantidade='14' fluid tipo='Total Exames'></Estatisticas>
+                            <Estatisticas quantidade='75' fluid tipo='Total Consultas'></Estatisticas>
+                            <Estatisticas quantidade='90' fluid tipo='Total Cadastros'></Estatisticas>
+                        </Col>
+
+                    </Col>
+                </Row>
+            </Container >
         </>
     )
 }
